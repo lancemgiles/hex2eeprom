6f428e928d2c427a30c498101fc7ee0848806f7f
lance <dogmandevices@gmail.com>
2024-05-13 15:54:26 -0500

    add comments instructing user to add alias to .bashrc, rewrtie script using aliases

--- hex2eeprom.sh
+++ hex2eeprom.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+# be sure to install srec_cat and ch341eeprom first
+# then, add the following to the bottom of .bashrc (located in your home directory)
+# alias ch341eeprom='/home/grz/dogmandevices/projects/FV-1/ch341eeprom/ch341eeprom'
+# if you don't do this, you will need to uncomment the middle line of the script and change 'ch341eeprom' to './ch341eeprom'
+
 # argument = filename without extention
 # convert .hex to .bin > to USB connected EEPROM
-# execute from project directory from terminal with sh /home/greze/FV-1/hex2eeprom/hex2eeprom.sh filename
+
+# execute from project directory from terminal with sh /path/to/hex2eeprom.sh filename
+# or add the following to .bashrc
+# alias hex2eeprom='/home/grz/dogmandevices/projects/FV-1/hex2eeprom/hex2eeprom.sh'
 
 srec_cat $1.hex -intel -o $1.bin -binary
-cd /home/greze/FV-1/ch341eeprom-master
-sudo ./ch341eeprom -s 24c32 -w /home/greze/FV-1/$1.bin
+# cd /path/to/ch341eeprom
+sudo ch341eeprom -s 24c32 -w /home/grz/dogmandevices/projects/FV-1/$1.bin
+# be sure to change the target directory to where you want the file to be saved
 
